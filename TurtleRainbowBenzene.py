e369d6d22473535f767afa2dbbac1902d0c5896f
jesimcz <jesim@seznam.cz>
2019-02-23 19:47:52 +0100

    initial commit

---
+++ TurtleRainbowBenzene.py
@@ -0,0 +1,16 @@
+
+# Python program to draw  
+# Rainbow Benzene 
+# using Turtle Programming
+# https://www.geeksforgeeks.org/turtle-programming-python/
+#
+import turtle 
+colors = ['red', 'purple', 'blue', 'green', 'orange', 'yellow'] 
+t = turtle.Pen() 
+turtle.bgcolor('black') 
+for x in range(360): 
+    t.pencolor(colors[x%6]) 
+    t.width(x/100 + 1) 
+    t.forward(x) 
+    t.left(59)
+
